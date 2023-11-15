123416b25070fa1fb71afbc8818d8144fbd44b8e
FireLord <firelord.xda@gmail.com>
2023-11-15 12:56:26 +0530

    Add query params

--- controllers/foods.js
+++ controllers/foods.js
@@ -1,7 +1,24 @@
 const Food = require("../models/food");
 
 const getAllFoods = async (req, res) => {
+    const { mealType, cuisineType, tag , q } = req.query;
     const queryObject = {};
+
+    if(mealType){
+        queryObject.mealType = { $regex: mealType, $options: "i" };
+    }
+
+    if(cuisineType){
+        queryObject.cuisineType = cuisineType;
+    }
+
+    if(tag){
+        queryObject.tag = tag;
+    }
+
+    if(q){
+        queryObject.label = { $regex: q, $options: "i" };
+    }
 
     let apiData = Food.find(queryObject);
 
