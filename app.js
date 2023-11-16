cc43b20c65d0d92260b8ef9572c7128315ec87ad
FireLord <firelord.xda@gmail.com>
2023-11-16 09:53:06 +0000

    use all the available ip interface

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use("/food/img",express.static(__dirname + '/data/food_img'));
 const start = async () => {
     try {
         await connectDB(process.env.MONGODB_URL);
-        app.listen(PORT, () => {
+        app.listen(PORT, '0.0.0.0', () => {
             console.log(`${PORT} Yes I am connected`);
         })
     } catch (error) {
