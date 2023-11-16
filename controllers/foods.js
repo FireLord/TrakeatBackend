2bcd740086e00431f81275ac440415ebfa5fc15a
FireLord <firelord.xda@gmail.com>
2023-11-16 09:53:25 +0000

    dont add port num for img

--- controllers/foods.js
+++ controllers/foods.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 const URL = process.env.HOST_URL
 
 const getImageURL = imageName => {
-    return `${URL}:${PORT}/food/img/${imageName}.jpg`;
+    return `${URL}/food/img/${imageName}.jpg`;
 };
 
 const getAllFoods = async (req, res) => {
